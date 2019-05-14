babd6b4d36111b2ed07bd927ccea8bccd2efca43
Bob White <robertouraccan@gmail.com>
2019-05-14 13:35:17 -0600

    def delete_old_bak added

--- backup.py
+++ backup.py
@@ -43,7 +43,28 @@ class MakeBackup():
             shutil.move(os.path.relpath(zp), self.dir_file_paste)
             print(f'moved {zp}')
 
+    def delete_old_bak(self, days_off):
+
+        os.chdir(self.dir_file_paste)
+        limit_date = datetime.datetime.now().date() - datetime.timedelta(days=days_off)
+
+        for f in glob.glob('*.zip'):
+            created_date = datetime.datetime.fromtimestamp(os.stat(f).st_mtime).date()
+            if created_date <= limit_date:    
+                os.remove(os.path.realpath(f))
+                print(f'bak {f} eliminado del respaldo')
+
+    def run(self, days_off=30):
+        print('ejecutando script de respaldo.')
+
+        self.delete_old_bak(days_off)
+        self.list_to_zip()
+        self.zip_list()
+        self.zip_move()
+
+        print('proceso finalizado.')
+
+    
 backup = MakeBackup("C:/", "C:/Users/Vaio/Desktop")
-print(backup.list_to_zip())
-backup.zip_list()
-backup.zip_move()
+
+backup.run()
